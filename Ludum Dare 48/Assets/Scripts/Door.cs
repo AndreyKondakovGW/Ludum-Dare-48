edef662859acfb6af5f39c3ae6e737b74d621117
nenapisannoe <58001221+nenapisannoe@users.noreply.github.com>
2021-04-25 10:12:33 +0300

    added movement and hints

---
+++ Ludum Dare 48/Assets/Scripts/Door.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private BoxCollider2D _box;
+    [SerializeField] GameObject textBox;
+
+    void Start()
+    {
+         _box = GetComponent<BoxCollider2D>();
+    }
+
+   void OnTriggerStay2D(Collider2D collision)
+    {
+        textBox.SetActive(true);
+        
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+       textBox.SetActive(false);
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Ludum Dare 48/Assets/Scripts/Note.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Note : MonoBehaviour   
+{
+    private BoxCollider2D _box;
+    [SerializeField] GameObject textBox;
+    [SerializeField] GameObject note;
+    void Start()
+    {
+            textBox.SetActive(false);
+            note.SetActive(false);
+          _box = GetComponent<BoxCollider2D>();
+    }
+
+   void OnTriggerEnter2D(Collider2D other)
+    {
+      textBox.SetActive(true);
+      
+    }
+      void OnTriggerStay2D(Collider2D other)
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            note.SetActive(true);
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+       textBox.SetActive(false);
+    }
+    void Update()
+    {
+        
+    }
+}
